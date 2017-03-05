4a0078b08a18fc0e15a7905a6fbaa19a13ed714c
vklein0 <vinklein@gmail.com>
2017-03-05 18:59:05 +0100

    - Exit code after test results

--- rundoctest.py
+++ rundoctest.py
@@ -1,7 +1,7 @@
 import ppl
 import doctest
 import sys
-res = doctest.testmod(ppl, optionflags=doctest.ELLIPSIS)
+res = str(doctest.testmod(ppl, optionflags=doctest.ELLIPSIS))
 print(res)
 offset = res.find("TestResults(failed=") + len("TestResults(failed=")
 sys.exit(int(res[offset:res.find(',', offset)]))
