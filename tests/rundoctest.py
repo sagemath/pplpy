fef959325fe166b5216fbfda722763e6c0c25d00
Vincent Delecroix <vincent.delecroix@labri.fr>
2017-03-06 23:49:56 +0100

    fix doctests

--- tests/rundoctest.py
+++ tests/rundoctest.py
@@ -2,6 +2,13 @@ import ppl
 import doctest
 import sys
 
-res = doctest.testmod(ppl.ppl, optionflags=doctest.ELLIPSIS|doctest.REPORT_NDIFF)
-print(res)
-sys.exit(res[0])
+print("Running doctests for pplpy")
+print('-'*80)
+ans = 0
+for mod in [ppl, ppl.wrappers]:
+    res = doctest.testmod(mod, optionflags=doctest.ELLIPSIS | doctest.REPORT_NDIFF)
+    print(mod)
+    print(res)
+    print('-'*80)
+    ans = ans | res[0]
+sys.exit(ans)
