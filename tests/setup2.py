8f7794b163c99b87f18b7f6e962ab2755f3fcde5
Vincent Klein <vinklein@gmail.com>
2017-03-08 22:28:20 +0100

    Adding a variant of cython call pplpy : include librairies is specified in the setup.py

---
+++ tests/setup2.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import sys
+import ppl
+
+extensions = [
+    Extension("testpplpy2", ["testpplpy2.pyx"],
+        include_dirs=ppl.__path__,
+        librairies=['ppl'],
+        )
+]
+
+setup(
+    name="test pplpy2",
+    ext_modules=cythonize(extensions, include_path=sys.path, language='c++')
+)
