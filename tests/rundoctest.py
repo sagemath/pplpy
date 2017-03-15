9dce301f569cc9a6157b2a9befda099b93e95b36
Vincent Klein <vinklein@gmail.com>
2017-03-15 16:07:22 +0100

    Test travis report with cython compile tests in rundoctest.py

--- tests/rundoctest.py
+++ tests/rundoctest.py
@@ -1,6 +1,16 @@
 import ppl
 import doctest
 import sys
+import subprocess
+
+#  - python setup.py build_ext --inplace
+#  - python -c "import testpplpy"
+#  - python setup2.py build_ext --inplace
+#  - python -c "import testpplpy2"
+subprocess.call([sys.executable, 'setup.py', 'build_ext', '--inplace'])
+subprocess.call([sys.executable, '-c', '"import testpplpy"'])
+subprocess.call([sys.executable, 'setup2.py', 'build_ext', '--inplace'])
+subprocess.call([sys.executable, '-c', '"import testpplpy2"'])
 
 print("Running doctests for pplpy")
 print('-'*80)
