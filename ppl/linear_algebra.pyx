48216ac8b5f60ebf62e47636ec1ff473cd2e8a46
vinklein <vincent.klein@u-bordeaux.fr>
2018-02-05 20:50:21 +0100

    license version : Update minimal GPL version from 2 to 3 (#29)
    Update minimal GPL version of extension from 2 to 3

--- ppl/linear_algebra.pyx
+++ ppl/linear_algebra.pyx
@@ -6,7 +6,7 @@
 #                     2017 Vincent Klein <vinklein@gmail.com>
 #
 #  Distributed under the terms of the GNU General Public License (GPL)
-#  as published by the Free Software Foundation; either version 2 of
+#  as published by the Free Software Foundation; either version 3 of
 #  the License, or (at youroption) any later version.
 #                  http://www.gnu.org/licenses/
 #*****************************************************************************
