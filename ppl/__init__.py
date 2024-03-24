64bc0c1cdf4d5b77f7e379a4dbee37fab80b6829
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-03-24 11:43:14 -0700

    Version 0.8.10

--- ppl/__init__.py
+++ ppl/__init__.py
@@ -127,7 +127,7 @@ AUTHORS:
   Remove the _mutable_immutable class.
 """
 
-__version__ = "0.8.10rc2"
+__version__ = "0.8.10"
 
 from .linear_algebra import (
         Variable, Variables_Set, Linear_Expression,
